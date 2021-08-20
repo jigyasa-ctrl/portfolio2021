e1e99c9882b8ac5499a01a6127d2c789a5ffd897
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-08-20 20:30:55 +0530

    homepage created

---
+++ src/Components/Homepage.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import logo from "../assets/logo.png"
+
+function Homepage() {
+    return (
+        <div className="homepage-div">
+         
+            <img className="img" src={logo} />
+            <h2 style={{fontWeight: 600}}>Jigyasa Upadhyay</h2>
+            <h3>I am a Frontend Developer @ Active.ai</h3>
+            <span></span>
+            <button>Hire Me</button>
+            <downicon></downicon>
+            
+        </div>
+    )
+}
+
+export default Homepage

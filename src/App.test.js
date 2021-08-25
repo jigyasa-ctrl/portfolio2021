85de35aa4a378773743750a808a2cc7b5b172bee
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-08-25 13:49:04 +0530

    More tetst added

--- src/App.test.js
+++ src/App.test.js
@@ -8,13 +8,32 @@
 
 import { shallow } from "enzyme";
 import React from "react";
+import { describe } from "yargs";
 import App from "./App"
 
-test("HomePage is rendered", () => {
-  // to check if component is there
-  const component = shallow(<App />)
-  // to find if homepage is there
-  const exists = component.find("Homepage").exists()
-  expect(exists).toBe(true);
 
-})
+
+  test("Homepage is rendered", () => { 
+    const component = shallow(<App />)
+    // to find if homepage is there
+    const exists = component.find("Homepage").exists()
+    expect(exists).toBe(true);
+  
+  })
+  
+  test("About is rendered", () => {
+    const component = shallow(<App />)
+    const aboutComp = component.find("About").exists()
+    expect(aboutComp).toBe(true)
+  
+  })
+ 
+  test("Projects is rendered", () => {
+    const component = shallow(<App />)
+    const project = component.find("Projects").exists()
+    expect(project).toBe(true)
+  
+  })
+ 
+  
+

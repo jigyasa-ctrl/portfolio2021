7f9e3cf175239690cd4b43a08561216d4c676484
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-08-25 08:42:39 +0530

    added carousal arrows

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Carousal from './Carousal'
+import leftArrow from '../assets/left-arrow.png'
+import rightArrow from '../assets/right-arrow.png'
 
 function Projects() {
     return (
         <div class="projects">
+            <img className="arrow" height="40px" src={leftArrow} />
             <Carousal />
             <Carousal />
             <Carousal />
+            <img className="arrow" height="40px" src={rightArrow} />
         </div>
     )
 }

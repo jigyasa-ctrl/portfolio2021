be9997d6ea3e5b8b9e6299de77f206a144da4d67
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-08-27 21:07:09 +0530

    Timeline added

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -6,15 +6,24 @@ function Experience() {
       <div className="container left">
         <div className="content">
           <h2>Frontend Developer</h2>
+          <h3>Active.Ai</h3>
+          <p>Jan-2021 to Present</p>
 
-          <h5>Active.Ai</h5>
+         
         </div>
       </div>
       <div className="container right">
         <div className="content">
-          <h2>Frontend Developer</h2>
-
-          <h5>Active.Ai</h5>
+          <h2>Full Stack Developer Intern</h2>
+          <h3>Learncool Ptd. Ltd.</h3>
+          <p>Dec-2020 to Jan-2021</p>
+        </div>
+      </div>
+      <div className="container left">
+        <div className="content">
+          <h2>Software Developer Intern</h2>
+          <h3>Playcent</h3>
+          <p>Aug-2020 to Dec-2020</p>
         </div>
       </div>
      

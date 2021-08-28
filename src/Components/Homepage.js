4c8d0dfa1232f2fc92dc80e48b2f76546170725d
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-08-28 13:23:04 +0530

    Changing styles

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -6,8 +6,8 @@ function Homepage() {
         <div className="homepage-div">
          
             <img className="img" src={logo} />
-            <h2 style={{fontWeight: 600}}>Jigyasa Upadhyay</h2>
-            <h3>I am a Frontend Developer @ Active.Ai</h3>
+            <h2 style={{fontWeight: 600, fontSize: '3rem'}}>Jigyasa Upadhyay</h2>
+            <h3 style={{fontWeight: 600, fontSize: '1.2rem'}}> Frontend Developer @ Active.Ai</h3>
             <span></span>
             <button>Hire Me</button>
             <downicon></downicon>

7030b5c1aeb326eba33f31679cd5c3f15e81904c
jigyasa-ctrl <jigyasaupadhyay60@gmail.com>
2021-08-29 14:02:50 +0530

    about modified

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,21 +1,36 @@
-import React from 'react'
-import linkedin from '../assets/linkedin.png'
-import github from '../assets/github.png'
-import email from '../assets/email.png'
-import medium from '../assets/medium.png'
+import React from "react";
+import linkedin from "../assets/linkedin.png";
+import github from "../assets/github.png";
+import email from "../assets/email.png";
+import medium from "../assets/medium.png";
 
 function Contact() {
-    return (
-        <>
-        <h1 id="contact-h1">Contact</h1>
-        <div className="contact-div">
-            <img src={linkedin}></img>
-            <img onClick={() => window.location.href ="https://github.com/jigyasa-ctrl"} src={github}></img>
-            <img src={email}></img> 
-            <img onClick={() => window.location.href ="https://medium.com/@jigyasaupadhyay60/"} src={medium}></img>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <h1 id="contact-h1">Contact</h1>
+      <div className="contact-div">
+        <img
+          onClick={() =>
+            (window.location.href =
+              " https://www.linkedin.com/in/jigyasa-upadhyay-24963515b/")
+          }
+          src={linkedin}
+        ></img>
+        <img
+          onClick={() =>
+            (window.location.href = "https://github.com/jigyasa-ctrl")
+          }
+          src={github}
+        ></img>
+        <img
+          onClick={() =>
+            (window.location.href = "https://medium.com/@jigyasaupadhyay60/")
+          }
+          src={medium}
+        ></img>
+      </div>
+    </>
+  );
 }
 
-export default Contact
+export default Contact;
--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -9,7 +9,8 @@ function Homepage() {
             <h2 style={{fontWeight: 600, fontSize: '3rem'}}>Jigyasa Upadhyay</h2>
             <h3 style={{fontWeight: 600, fontSize: '1.2rem'}}> Frontend Developer @ Active.Ai</h3>
             <span></span>
-            <button>Hire Me</button>
+            <button><a style={{textDecoration: 'none', color: 'inherit'}} href="mailto: jigyasaupadhyay60@gmail.com">Hire Me</a></button>
+            
             <downicon></downicon>
             
         </div>
